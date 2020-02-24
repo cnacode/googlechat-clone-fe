38ce1321a1508e1fa300b3a630ba30f22cc1081e
Sina Montazeri <accounts@sinamontazeri.com>
2020-02-24 20:11:02 +0330

    fix routing errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HomeComponent } from '@app/home';
+import { AuthGuard } from '@app/core/authentication';
+import { LoginComponent } from '@app/login';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  {path: '/login', component: LoginComponent},
-
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
@@ -15,4 +16,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
