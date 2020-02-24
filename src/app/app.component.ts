a3d9d4202e3b37dfea6298debd801c58cf237877
Sina Montazeri <accounts@sinamontazeri.com>
2020-02-24 20:12:03 +0330

    fix authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from '@app/core/models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '@app/core/authentication';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentUser: User;
+
+  constructor(private router: Router, private authenticationService: AuthenticationService) {
+    this.authenticationService.currentUser.subscribe(this.login);
+  }
+
+  private login(authenticatedUser) {
+    this.currentUser = authenticatedUser;
+  }
+
+  public logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
   title = 'fe-message-board';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { AuthenticationComponent } from './authentication/authentication.component';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { mockAuthenticationProvider } from '@app/core/mocks';
+import { GridComponent } from '@app/shared/grid/grid.component';
+import { LoginComponent } from '@app/login/login.component';
+import { HomeComponent } from '@app/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent,
-    AuthenticationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, GridComponent, LoginComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
+  providers: [mockAuthenticationProvider],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
