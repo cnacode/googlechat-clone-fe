eb9a4bb70bd788c41bdf069b9f101ee0d35fe196
Sina Montazeri <accounts@sinamontazeri.com>
2020-03-08 23:00:02 +0330

    update styled button component

---
+++ src/app/shared/styled-button/button.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Button } from 'protractor';
+
+export enum ButtonStyles {
+  Primary = 'primary',
+  Secondary = 'secondary'
+}
+@Component({
+  selector: 'styled-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class StyledButtonComponent implements OnInit {
+  @Input()
+  style: ButtonStyles = ButtonStyles.Primary;
+
+  @Input()
+  size: string;
+
+  @Input()
+  type: string;
+
+  isPrimary: boolean;
+  isWide: boolean;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    //set isPrimary
+    this.isPrimary = this.style === ButtonStyles.Primary;
+    this.isWide = this.size == 'wide';
+  }
+}
