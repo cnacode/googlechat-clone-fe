949fd630eb29b5e364a8a6dbfc8a39ae686f19c8
Sina Montazeri <accounts@sinamontazeri.com>
2020-03-29 00:46:26 +0430

    implement send reply

--- src/app/components/new-reply/new-reply.component.ts
+++ src/app/components/new-reply/new-reply.component.ts
@@ -1,26 +1,62 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from '@app/core/services';
 
 @Component({
   selector: 'new-reply',
   templateUrl: './new-reply.component.html',
   styleUrls: ['./new-reply.component.scss']
 })
-export class NewReplyComponent implements OnInit {
+export class NewReplyComponent {
   @Input()
   parentId: string;
 
   messageForm: FormGroup;
   loading = false;
+  error: string = '';
+  success: string = '';
+
   textareaOptions = {
     hideLabel: true
   };
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private messageService: MessageService) {
     this.messageForm = this.formBuilder.group({
       message: ['', [Validators.required]]
     });
   }
 
   ngOnInit(): void {}
+
+  onSubmit(): void {
+    const validate = () => {
+      const { invalid } = this.messageForm;
+      if (invalid) {
+        const { errors } = this.messageForm.get('message');
+        if (errors?.required) {
+          this.error = 'Reply can not be empty.';
+        }
+        return false;
+      }
+      return true;
+    };
+    const handleError = error => {
+      console.log(error);
+      this.error = error;
+      this.loading = false;
+    };
+    const handleSuccess = () => {
+      this.loading = false;
+      this.messageService.getMessages(0).subscribe();
+      const newMessageTextArea = this.messageForm.get('message');
+      newMessageTextArea.setValue('');
+      this.success = 'Reply Posted!';
+    };
+
+    const valid = validate();
+    if (!valid) return;
+    this.loading = true;
+    const { message } = this.messageForm.getRawValue();
+    this.messageService.createMessage(message, this.parentId).subscribe(handleSuccess, handleError);
+  }
 }
