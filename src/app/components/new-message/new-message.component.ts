98bb2f497736129f7a9e47236cd00748f98141f8
Sina Montazeri <accounts@sinamontazeri.com>
2020-03-29 00:45:59 +0430

    add new message

--- src/app/components/new-message/new-message.component.ts
+++ src/app/components/new-message/new-message.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from '@app/core/services';
 
 @Component({
   selector: 'new-message',
@@ -9,15 +10,48 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class NewMessageComponent implements OnInit {
   messageForm: FormGroup;
   loading = false;
+  error: string = '';
+
   textareaOptions = {
     hideLabel: true
   };
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private messageService: MessageService) {
     this.messageForm = this.formBuilder.group({
       message: ['', [Validators.required]]
     });
   }
 
   ngOnInit(): void {}
+
+  onSubmit(): void {
+    const handleError = error => {
+      this.error = error;
+      this.loading = false;
+    };
+    const handleSuccess = () => {
+      this.loading = false;
+      this.messageService.getMessages(0).subscribe();
+      const newMessageTextArea = this.messageForm.get('message');
+      newMessageTextArea.setValue('');
+    };
+    const validate = () => {
+      const { invalid } = this.messageForm;
+      if (invalid) {
+        const { errors } = this.messageForm.get('message');
+        if (errors?.required) {
+          this.error = 'reply can not be empty';
+        }
+        return false;
+      }
+      return true;
+    };
+
+    const valid = validate();
+    if (!valid) return;
+
+    this.loading = true;
+    const { message } = this.messageForm.getRawValue();
+    this.messageService.createMessage(message).subscribe(handleSuccess, handleError);
+  }
 }
