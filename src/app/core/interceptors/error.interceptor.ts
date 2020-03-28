b6f89d03a897cd5f891470ae3dea42751d3293c7
Sina Montazeri <accounts@sinamontazeri.com>
2020-03-29 00:45:45 +0430

    improve error handler

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 import { AuthenticationService } from '@app/core/services';
 
@@ -11,7 +11,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
-      catchError(response => {
+      catchError((response: HttpErrorResponse) => {
         if (response.status === 403) {
           // auto logout if 403 response returned from api
           this.authenticationService.logout();
@@ -19,7 +19,6 @@ export class ErrorInterceptor implements HttpInterceptor {
         }
 
         const error = response.error.message || response.statusText;
-        console.log(response.error);
         return throwError(error);
       })
     );
